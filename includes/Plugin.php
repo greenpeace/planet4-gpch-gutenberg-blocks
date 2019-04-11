6e7ab20e8a07acb5ed162d28db8d2c6d8fe3b456
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-04-11 11:19:53 +0200

    Changed block loading to be more versatile

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -15,6 +15,13 @@ if ( ! class_exists( 'Planet4_Gpch_Gutenberg_Blocks' ) ) {
 		private static $instance;
 
 		/**
+		 * Block instances
+		 *
+		 * @var $blocks
+		 */
+		private $blocks;
+
+		/**
 		 * Returns the instance
 		 *
 		 * @return Planet4_Gpch_Gutenberg_Blocks
@@ -32,25 +39,29 @@ if ( ! class_exists( 'Planet4_Gpch_Gutenberg_Blocks' ) ) {
 		 */
 		private function __construct() {
 			// Output an error message in case ACF isn't installed.
-			if ( ! class_exists('ACF') ) {
-				add_action( 'admin_notices', array( $this, 'error_message_no_acf') );
+			if ( ! class_exists( 'ACF' ) ) {
+				add_action( 'admin_notices', array( $this, 'error_message_no_acf' ) );
 			}
 
 			// Actions
-			add_action( 'admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
 
 			// Load Blocks
-			$BlockGallerySlider = new Blocks\Planet4_Gutenberg_Block_Gallery_Slider();
+			$this->blocks = array(
+				new Blocks\Planet4_Gutenberg_Block_Gallery_Slider(),
+				new Blocks\Planet4_Gutenberg_Block_Gallery_3Column(),
+				new Blocks\Planet4_Gutenberg_Block_Gallery_Grid(),
+			);
 		}
 
 		/**
 		 * Outputs an error message in Wordpress admin
 		 */
-		public  function error_message_no_acf() {
+		public function error_message_no_acf() {
 			?>
-			<div class="error notice">
-				<p><?php _e( 'Planet 4 GPCH Gutenberg Blocks: Advanced Custom Fields must be installed and activated for this plugin to work.', 'planet4-gutenberg-blocks' ); ?></p>
-			</div>
+            <div class="error notice">
+                <p><?php _e( 'Planet 4 GPCH Gutenberg Blocks: Advanced Custom Fields must be installed and activated for this plugin to work.', 'planet4-gutenberg-blocks' ); ?></p>
+            </div>
 			<?php
 		}
 
